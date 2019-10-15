27fe79cb4464477c821416e6e79d11b4a76420a7
melpst <jitlada.su@ku.th>
2019-10-15 11:25:31 +0700

    add vuetify into config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+const colors = require('vuetify/es5/util/colors').default
 
 module.exports = {
   mode: 'spa',
@@ -36,7 +37,8 @@ module.exports = {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
   ],
   /*
   ** Nuxt.js modules
@@ -50,6 +52,27 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+  },
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   /*
   ** Build configuration
